7aa4d0068208c4c1f7edbcf1fe89204255af7852
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2021-03-17 07:19:02 +0000

    ready to deploy

--- public/connectPDO.php
+++ public/connectPDO.php
@@ -1,13 +1,3 @@
 <?php
-$server ='v.je';
-$user = 'student';
-$pass = 'student';
-$schema = 'csy2028';
-
-// connect to the Data base with the credentials above
-// this file must be included on al pages
-
-$pdo = new PDO('mysql:dbname=' .$schema. ';host=' . $server, $user, $pass,
- [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-?>
+$pdo = new PDO('mysql:host=freedb.tech;dbname=freedbtech_jokesite;charset=utf8', 'freedbtech_jokesite', 'jokesite');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
